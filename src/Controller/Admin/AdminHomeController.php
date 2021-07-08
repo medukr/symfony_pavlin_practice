f51b6f00453ae6d7c118bed4780dc2df10b3a9b5
Andrii Demydiuk <andrii.demydyuk@gmail.com>
2021-07-08 16:42:32 +0300

    less 1 - added base controllers

---
+++ src/Controller/Admin/AdminHomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminHomeController extends AdminBaseController
+{
+    #[Route('/admin', name: 'admin_home')]
+    public function index(): Response
+    {
+        $forRender = parent::renderDefault();
+
+        return $this->render('admin/index.html.twig', $forRender);
+    }
+}
---
+++ src/Controller/Main/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller\Main;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends BaseController
+{
+    #[Route('/', name: 'home')]
+    public function index(): Response
+    {
+        $forRender = $this->renderDefault();
+        return $this->render('main/index.html.twig', $forRender);
+    }
+}
