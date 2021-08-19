34aabb4c57e61ccbc480605c22443c97cf0f0587
Andrii Demydiuk <andrii.demydyuk@gmail.com>
2021-08-19 22:47:09 +0300

    less 6 - Post, Comment entities

---
+++ src/Entity/Post.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PostRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PostRepository::class)
+ */
+class Post
+{
+    private const PUBLISHED = 1;
+    private const DRAFT     = 0;
+
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=1000)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="string", length=500, nullable=true)
+     */
+    private $image;
+
+    /**
+     * @ORM\Column(type="datetime_immutable")
+     */
+    private $cteate_at;
+
+    /**
+     * @ORM\Column(type="datetime_immutable")
+     */
+    private $update_at;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $is_published;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getCteateAt(): ?\DateTimeImmutable
+    {
+        return $this->cteate_at;
+    }
+
+    public function setCrateAtValue()
+    {
+        $this->cteate_at = new \DateTimeImmutable();
+    }
+
+    public function setCteateAt(\DateTimeImmutable $cteate_at): self
+    {
+        $this->cteate_at = $cteate_at;
+
+        return $this;
+    }
+
+    public function getUpdateAt(): ?\DateTimeImmutable
+    {
+        return $this->update_at;
+    }
+
+    public function setUpdateAtValue()
+    {
+        $this->update_at = new \DateTimeImmutable();
+    }
+
+    public function setUpdateAt(\DateTimeImmutable $update_at): self
+    {
+        $this->update_at = $update_at;
+
+        return $this;
+    }
+
+    public function getIsPublished(): ?bool
+    {
+        return $this->is_published;
+    }
+
+    public function setIsPublished()
+    {
+        $this->is_published = self::PUBLISHED;
+    }
+
+    public function setIsDraft()
+    {
+        $this->is_published = self::DRAFT;
+    }
+
+}
